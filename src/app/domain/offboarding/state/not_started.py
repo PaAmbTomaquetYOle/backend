3e9b478b7485258716e4f3b820e4adc6e11d876b
Oriol Orbea <oriol.orbea@gmail.com>
2026-06-27 19:32:55 +0200

    Add type checking for offboarding process states and implement start method in NotStartedState

--- src/app/domain/offboarding/state/not_started.py
+++ src/app/domain/offboarding/state/not_started.py
@@ -1,5 +1,10 @@
-from app.domain.enums import OffboardingProcessStateEnum
-from app.domain.offboarding.state import OffboardingProcessState
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from app.domain.enums import OffboardingProcessStateEnum
+    from app.domain.offboarding.state import InProgressState, OffboardingProcessState
 
 
 class NotStartedState(OffboardingProcessState):
@@ -10,3 +15,5 @@ class NotStartedState(OffboardingProcessState):
     def get_state(self) -> OffboardingProcessStateEnum:
         return OffboardingProcessStateEnum.NOT_STARTED
 
+    def start(self) -> OffboardingProcessState:
+        return InProgressState()
