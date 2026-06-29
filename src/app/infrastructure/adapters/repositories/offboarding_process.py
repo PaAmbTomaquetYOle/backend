c980e424b0a5a97318814f9df99f4a9cd70c8d39
Oriol Orbea <oorbea@pasiona.com>
2026-06-29 09:59:13 +0200

    Add IOffboardingProcessRepository interface and update exports in __init__.py

---
+++ src/app/infrastructure/adapters/repositories/offboarding_process.py
@@ -0,0 +1,7 @@
+from app.application import IOffboardingProcessRepository
+
+
+class OffboardingProcessRepository(IOffboardingProcessRepository):
+    """
+    Concrete implementation of the offboarding process repository.
+    """
